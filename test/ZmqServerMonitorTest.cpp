7068d50b7090db9aba648a68af113c64fd89cf26
Eric Pederson <ericacm+github@gmail.com>
2016-11-27 01:04:09 -0500

    Add ZMQServerMonitor and ZmqServerMonitorTest.

--- test/ZmqServerMonitorTest.cpp
+++ test/ZmqServerMonitorTest.cpp
@@ -5,12 +5,16 @@ TEST_CASE("Connect", "[ZMQ][ZmqServerMonitor][Connect]") {
     zmq::context_t context;
     zmq::socket_t server(context, ZMQ_ROUTER);
     server.bind("tcp://localhost:5050");
-    zmq::socket_t parent(context, ZMQ_REP);
-    const std::string ParentEndpoint{"inproc://test.alertee"};
-    parent.bind(ParentEndpoint.c_str());
+    zmq::socket_t alertee(context, ZMQ_REP);
+    const std::string AlerteeEndpoint{"inproc://test.alertee"};
+    alertee.bind(AlerteeEndpoint.c_str());
 
     ZmqServerMonitor serverMonitor(context, ServerType::Primary, "localhost", 5050,
-                                   ParentEndpoint, 0, std::chrono::seconds(2));
+                                   AlerteeEndpoint, 0, std::chrono::seconds(2));
+    zmq::pollitem_t items[] = {
+            {server, 0, ZMQ_POLLIN, 0},
+            {alertee, 0, ZMQ_POLLIN, 0}
+    };
 
-
+    zmq::poll(items, 2, -1);
 }
